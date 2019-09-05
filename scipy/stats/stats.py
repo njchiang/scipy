7e89021f2c27d7c50b59f22b41b816ce0b2e6b35
njchiang <jeff.njchiang@gmail.com>
2019-09-04 22:55:06 -0700

    fix edge case where number of nans in axis is greater than n_vars

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -3767,7 +3767,7 @@ def spearmanr(a, b=None, axis=0, nan_policy='propagate'):
             else:
                 # Keep track of variables with NaNs, set the outputs to NaN
                 # only for those variables
-                variable_has_nan = np.isnan(a).sum(axis=axisout)
+                variable_has_nan = np.isnan(a).sum(axis=axisout).astype(np.bool)
 
     a_ranked = np.apply_along_axis(rankdata, axisout, a)
     rs = np.corrcoef(a_ranked, rowvar=axisout)
